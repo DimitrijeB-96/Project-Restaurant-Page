f9e341997c5aaf261991889eb926de4f204e8989
DimitrijeB-96 <mitabackalic96@gmail.com>
2023-06-16 13:35:36 +0200

    Home and About Us pages completed

--- src/pages/aboutUs.js
+++ src/pages/aboutUs.js
@@ -1,7 +1,67 @@
 const aboutUsPage = document.createElement('div');
 
-export function displayAboutUsPage() {
+const contactDiv = document.createElement('div');
+const contactH2 = document.createElement('h2');
+const contactWorkingDays = document.createElement('h3');
+const contactWorkingHours = document.createElement('h3');
+const contactLocation = document.createElement('p');
+const contactPhone = document.createElement('p');
+const contactEmail = document.createElement('p');
+
+const aboutDiv = document.createElement('div');
+const aboutH2 = document.createElement('h2');
+const notRealProjectText = document.createElement('p');
+const projectGoalText = document.createElement('p');
+const projectBackgroundAuthorText = document.createElement('p');
+const projectBackgroundAuthorLink = document.createElement('a');
+
+function displayContactDiv() {
+  aboutUsPage.appendChild(contactDiv);
+  contactDiv.classList.add('contact-div-aboutUs');
+
+  contactDiv.appendChild(contactH2);
+  contactH2.textContent = 'Contact';
+
+  contactDiv.appendChild(contactWorkingDays);
+  contactWorkingDays.textContent = 'Monday - Sunday';
+
+  contactDiv.appendChild(contactWorkingHours);
+  contactWorkingHours.textContent = '11:00 - 23:00';
   
+  contactDiv.appendChild(contactLocation);
+  contactLocation.textContent = 'Location: 123 Not Real Place, Thera Greece';
+
+  contactDiv.appendChild(contactPhone);
+  contactPhone.textContent = 'Phone number: +30 123 456 7890';
+
+  contactDiv.appendChild(contactEmail);
+  contactEmail.textContent = 'Email: horizon@restaurant-project.com';
+}
+
+function displayAboutUsDiv() {
+  aboutUsPage.appendChild(aboutDiv);
+  aboutDiv.classList.add('about-div-aboutUs');
+
+  aboutDiv.appendChild(aboutH2);
+  aboutH2.textContent = 'About Us';
+  aboutDiv.appendChild(notRealProjectText);
+  notRealProjectText.textContent = 'We are not real restaurant!';
+  aboutDiv.appendChild(projectGoalText);
+  projectGoalText.textContent = 'This is just a "project" from the Odin Project curriculum. Main goal of this project is to learning how to implement npm and webpack, with having header navigation bar.';
+
+  aboutDiv.appendChild(projectBackgroundAuthorText);
+  projectBackgroundAuthorText.textContent = 'Background picture from ';
+
+  projectBackgroundAuthorText.appendChild(projectBackgroundAuthorLink);
+  projectBackgroundAuthorLink.textContent = 'Angela Pham';
+  projectBackgroundAuthorLink.href = 'https://www.unsplash.com/@apham';
+  projectBackgroundAuthorLink.setAttribute('target', '_blank');
+}
+
+export function displayAboutUsPage() {
+  displayContactDiv();
+  displayAboutUsDiv();
+  aboutUsPage.classList.add('aboutUs');
 }
 
 export default aboutUsPage;
--- src/pages/home.js
+++ src/pages/home.js
@@ -6,8 +6,12 @@ const heroText = document.createElement('p');
 const heroSpan = document.createElement('span');
 
 const contentDiv = document.createElement('div');
-const contentH2 = document.createElement('h2');
-const contentText = document.createElement('p');
+const contentH2First = document.createElement('h2');
+const contentTextIntro = document.createElement('p');
+const contentH2Second = document.createElement('h2');
+const contentText1 = document.createElement('p');
+const contentText2 = document.createElement('p');
+const contentText3 = document.createElement('p');
 
 function displayHeroDiv() {
   homePage.appendChild(heroDiv);
@@ -26,10 +30,19 @@ function displayContentDiv() {
   homePage.appendChild(contentDiv);
   contentDiv.classList.add('content-div-home');
 
-  contentDiv.appendChild(contentH2);
-  contentH2.textContent = 'Our Story';
-  contentDiv.appendChild(contentText);
-  contentText.textContent = 'Welcome to our restaurant...';
+  contentDiv.appendChild(contentH2First);
+  contentH2First.textContent = 'Our Story';
+  contentDiv.appendChild(contentTextIntro);
+  
+  contentTextIntro.textContent = 'Horizon opened in 1988 with the goal of providing traditional homemade meals, with the emphasis on quality.';
+  contentDiv.appendChild(contentH2Second);
+  contentDiv.appendChild(contentText1);
+  contentDiv.appendChild(contentText2);
+  contentDiv.appendChild(contentText3);
+  contentH2Second.textContent = 'Visit Us';
+  contentText1.textContent = 'At Horizon, we offer local Greek cuisine and traditional flavors that you can only eat at our restaurant in Santorini.';
+  contentText2.textContent = 'With great food you can enjoy the best Greek wine from local Wineyards, to bring richnest and color to your food.';
+  contentText3.textContent = 'To top it all up, we offer best views on the island, there is no excuse not to visit us!';
 }
 
 export function displayHomePage() {
